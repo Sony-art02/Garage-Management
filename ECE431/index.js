b0e8118a8861647e7b0e1f1359bf9460d84b91c2
Sony-art02 <amaladasusony322005@gmail.com>
2025-07-18 15:55:32 +0530

    Add files via upload

---
+++ ECE431/index.js
@@ -0,0 +1,13 @@
+
+let person={name:"sony",age:20 };
+console.log("object:", person,"|type:",typeof person);
+
+let fruit=["Apple","banana","mango"];
+console.log("Array:",fruit,"|type:",typeof fruit);
+
+function hello(){
+    console.log("hey");
+
+}
+console.log("fution:",hello,"|type:",typeof hello);
+hello();
